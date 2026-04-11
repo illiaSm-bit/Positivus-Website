1c4f739ec718e030133a8687f5256990b17d8967
illia <smahliukillia927@gmail.com>
2026-04-11 13:53:20 +0200

    i adapted working process part for mobile devices + fixed bugs

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const processBlocks = document.querySelectorAll(".process_block ");
 
 processBlocks.forEach((block) => {
   const btn = block.querySelector(".btn_process");
+  const btnImg = block.querySelector(".btn_img");
 
   btn.addEventListener("click", () => {
     block.classList.toggle("process_block_isActive");
+    if (block.classList.contains("process_block_isActive")) {
+      btnImg.src = "./assets/icons/-.svg";
+    } else {
+      btnImg.src = "./assets/icons/+.svg";
+    }
   });
 });
